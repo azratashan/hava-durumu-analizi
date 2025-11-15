0b600e9473ac8290e2e56dc1ca79c97aa0db0e68
azratashan <azratashan.24@gmail.com>
2025-11-15 12:21:52 +0300

    Proje 2 - Aşama 3 Tamamlandı: Aylık Trend Görselleştirmesi Eklendi.

--- hava_analizi.py
+++ hava_analizi.py
@@ -2,22 +2,61 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- AŞAMA 1: VERİ YÜKLEME VE HAZIRLIK (Noktalı Virgül (;) Hatası Düzeltildi) ---
 
-# 1. Veri Setini Yükleme
 try:
-    # sep=';' parametresini ekleyerek dosyanın doğru ayrılmasını sağlıyoruz.
+    
     df = pd.read_csv('munich.csv', sep=';') 
     print("Veri seti başarıyla yüklendi.")
 except FileNotFoundError:
     print("HATA: Dosya bulunamadı. Lütfen dosya adını ve yolunu kontrol edin.")
     
-# 2. Tarih Sütununu Düzeltme (Hata çözüldü, satırı tekrar açtık)
+
 df['time'] = pd.to_datetime(df['time'])
 
-# 3. İlk Veri Keşfi ve Kontrol
+
 print("\n--- İlk 5 Satır ---")
 print(df.head()) 
 
 print("\n--- Veri Seti Bilgi Özeti ---")
-df.info() # Veri tiplerini, eksik veri sayısını gösterir
+df.info() 
+
+df['precipitation_sum (mm)'] = df['precipitation_sum (mm)'].fillna(0)
+df['snowfall_sum (cm)'] = df['snowfall_sum (cm)'].fillna(0)
+
+
+print("\n--- Eksik Veri Doldurulduktan Sonraki Kontrol ---")
+df.info()
+
+
+df['year'] = df['time'].dt.year
+
+
+yillik_ortalama = df.groupby('year')['precipitation_sum (mm)'].mean()
+
+print("\n--- Yıllık Ortalama Yağış Miktarları ---")
+print(yillik_ortalama)
+
+
+df['month'] = df['time'].dt.month
+
+
+aylik_ortalama = df.groupby('month')[['precipitation_sum (mm)', 'snowfall_sum (cm)']].mean()
+
+
+plt.figure(figsize=(10, 6))
+
+
+plt.plot(aylik_ortalama.index, aylik_ortalama['precipitation_sum (mm)'], 
+         marker='o', linestyle='-', color='skyblue', label='Ortalama Yağış (mm)')
+
+
+plt.plot(aylik_ortalama.index, aylik_ortalama['snowfall_sum (cm)'], 
+         marker='x', linestyle='--', color='darkblue', label='Ortalama Kar (cm)')
+
+plt.title('2024 Yılı Aylık Ortalama Yağış ve Kar Miktarları')
+plt.xlabel('Ay')
+plt.ylabel('Miktar (mm/cm)')
+plt.xticks(aylik_ortalama.index) 
+plt.legend()
+plt.grid(True)
+plt.show()
